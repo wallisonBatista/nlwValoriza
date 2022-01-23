56810ca77abbfe81c74e21979872cbf79c0b2bb8
wallisonBatista <wallisongbatista@gmail.com>
2022-01-23 16:40:12 -0300

    biblioteca uuid adicionada e configuração do BD

---
+++ src/entities/User.ts
@@ -0,0 +1,38 @@
+import {
+    Entity,
+    PrimaryColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    Column,
+} from 'typeorm';
+import { v4 as uuid } from "uuid";
+
+@Entity('users')
+class User {
+
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    create_at: Date;
+
+    @UpdateDateColumn()
+    update_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { User };
