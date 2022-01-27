fb32fab9aa8958f076a36d1799adc0c9cac0aefe
wallisonBatista <wallisongbatista@gmail.com>
2022-01-27 20:29:09 -0300

    createUser service criado

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+    name: string
+    email: string
+    admin?: boolean
+}
+
+class CreateUserService {
+    async execute({ name, email, admin }: IUserRequest) {
+        const usersRepository = new UsersRepositories();
+
+        //verificar se o e-mail está preenchido
+        if (!email) {
+            throw new Error("Email incorrect")
+        }
+
+        //verifica se o usuário já existe (e-mail)
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        })
+        //se o e-mail já existir lança uma exceção
+        if (userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        });
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService }
